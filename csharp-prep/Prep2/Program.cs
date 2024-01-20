cf8d8bb46e68d01945d92bf9b4a0d22535faa6cd
Nate Green <bal16004@byui.edu>
2024-01-20 15:32:38 -0700

    Finished prep 2 assignment, but did not do stretch.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Define Variables
+        float grade;
+        string letterGrade;
+        bool pass;
+
+        // Get User input
+        Console.Write($"Please input your grade percentage: ");
+        grade = Console.Read();
+
+        // Determine Grade and if they passed
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+            pass = true;
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+            pass = true;
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+            pass = true;
+        } 
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+            pass = false;
+        }
+        else
+        {
+            letterGrade = "F";
+            pass = false;
+        }
+
+        // Print out grade and if they passed
+        Console.WriteLine($"You got a[n] {letterGrade} in the class." );
+        if (pass)
+        {
+            Console.WriteLine($"You Passed. Congrations.");
+        }
+        else
+        {
+            Console.WriteLine($"You Failed. Outer Darkness.");
+        }
+
     }
 }
