36a086b5178e6ef7bf362ed461606a2ec82be051
Nate Green <bal16004@byui.edu>
2024-01-20 16:47:58 -0700

    Finished prep 3 assignment along with stretch goal.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,75 @@
 using System;
+using System.Globalization;
 
 class Program
 {
+    static void GuessingGame(int number, int tries, bool win)
+    {
+        // Define Variables
+        int guess;
+
+        // Loop Guessing Game
+        do
+        {
+            // Ask User for Random Number
+            Console.Write($"What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess == number)
+            {
+                Console.WriteLine($"You guessed it!");
+                Console.WriteLine($"This game took {tries} guesses. ");
+                win = true;
+            }
+            else if (guess > number)
+            {
+                tries += 1;
+                Console.WriteLine($"Lower");
+            }
+            else
+            {
+                tries += 1;
+                Console.WriteLine($"Higher");
+            }
+        } while (!win);
+
+        return;
+    }
+    
+    
+    
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Define Variables
+        int tries = 0;
+        bool win = false;
+        bool playAgain = true;
+
+        // Run Guessing Game
+        do
+        {
+            // Define a Random Number
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1,101);
+
+            // Run Actual game
+            GuessingGame(number,tries,win);
+
+            // See if player wants to play again
+            Console.Write($"Play again? (y/n): ");
+            string reply = Console.ReadLine();
+            if (reply == "y")
+            {
+                Console.WriteLine($"==================");
+                playAgain = true;
+            }
+            else
+            {
+                playAgain = false;
+            }
+
+        } while (playAgain);
+        
     }
 }
