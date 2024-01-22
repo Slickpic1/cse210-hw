c853907b8c0f9fd3c2869f1c8c20d921f379a27b
Nate Green <bal16004@byui.edu>
2024-01-21 19:33:04 -0700

    Finished prep 4. Found some interesting problems with not using readline.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Define Variables/ lists
+        int sum = 0;
+        int userNumber = -1;
+        int largest = 0;
+        float avg;
+        List<int> numbers = new List<int>();
+
+        // Ask user to input list of numbers
+        Console.WriteLine($"Enter a list of numbers, type 0 when finished.");
+
+        // Write into list
+        while (userNumber != 0)
+        {
+            Console.Write($"Enter number: ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            //Check to see if number is zero or not
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        // Sum list
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        //Print Sum
+        Console.WriteLine($"The sum is: {sum}");
+
+        //calculate and print average
+        avg = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {avg}");
+
+        //Find and print the largest value
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+                largest = number;
+        }
+
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
+
