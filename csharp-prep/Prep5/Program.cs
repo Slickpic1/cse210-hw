ddee0bd23c09cd314d21bfd7273a53f268dc76fb
Nate Green <bal16004@byui.edu>
2024-01-29 09:25:39 -0700

    Finished Prep5, finally.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,59 @@
 using System;
+using System.Globalization;
 
 class Program
 {
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine($"Welcome to the program!");
+        return;
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write($"Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static float FaveNumber()
+    {
+        Console.Write($"Please enter your favorite number: ");
+        string userResponse = Console.ReadLine();
+        float favNum = float.Parse(userResponse);
+        return favNum;
+    }
+
+    static float SquareNumber(float number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string userName, float square)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {square}.");
+        return;
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Define vars
+        string username;
+        float number;
+        float square;
+
+        // Display the Program welcome
+        DisplayWelcome();
+
+        // Prompt for username
+        username = PromptUserName();
+
+        // Prompt user for favorite number
+        number = FaveNumber();
+
+        //Square number and display it
+        square = SquareNumber(number);
+        DisplayResult(username, square);
     }
 }
