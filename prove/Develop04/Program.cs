65d2a78465cb94eec0881a4ab22085160acc6226
Nate Green <bal16004@byui.edu>
2024-02-26 09:28:42 -0700

    Removed old Develop04 files, and replaced Program file with new one.

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -2,37 +2,12 @@ using System;
 
 class Program
 {
-    static bool DisplayPrompt(Scripture scrip)
+    static void DisplayMenu()
     {
-        scrip.DisplayScripture();
-        Console.WriteLine($"Press enter to continue or type 'quit' to finish:");
-        string response = Console.ReadLine();
-
-        if (response == "quit")
-        {
-            return false;
-        }
-        else
-        {
-            Console.Clear();
-            return true;
-        }
+        
     }
-
     static void Main(string[] args)
     {
-        //Run the program
-
-        //Initialize and display the scripture
-        Scripture scrip = new Scripture();
-        bool running = DisplayPrompt(scrip);
-
-        while (!scrip.IsAllBlank() && running)
-        {
-            //Erase words
-            scrip.EraseWords();
-            running = DisplayPrompt(scrip);
-            
-        }
+        ;
     }
 }
