a57693abbed98efbfce8d79b8845869d375da4b9
Nate Green <bal16004@byui.edu>
2024-03-06 22:41:55 -0700

    Got the rough program finished.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square square = new Square("White",12);
+        Rectangle rectangle = new Rectangle("Red",12,16);
+        Circle circle = new Circle("Blue",12);
+        
+        double squareArea = square.GetArea();
+        double rectangleArea = rectangle.GetArea();
+        double circleArea = circle.GetArea();
+
+        shapes.Add(square);
+        shapes.Add(circle);
+        shapes.Add(rectangle);
+
+        Console.WriteLine(shapes[0].GetArea());
     }
 }
