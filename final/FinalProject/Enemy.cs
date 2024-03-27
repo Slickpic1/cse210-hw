a7c4aeb5f8b0b7195863ebc2f45f1fccaae94501
Nate Green <bal16004@byui.edu>
2024-03-27 13:41:03 -0600

    World reads file data from different files.

--- final/FinalProject/Enemy.cs
+++ final/FinalProject/Enemy.cs
@@ -1,6 +1,17 @@
 namespace Adventure_Qwest;
 public class Enemy : Entity
 {
+    private string name;
+    private string type;
+    private List<List<string>> enemyData = new List<List<string>> //potentially change to dict or array of arrays if thats better
+    {
+        //General structure
+        //{name,type,health,baseDamage,}  <- Need to restructure this for DnD style stats
+        new List<string>{"goblin","7","5"},
+        new List<string>{""}
+
+    };
+
     public Enemy(int[] position) : base(position)
     {
         
